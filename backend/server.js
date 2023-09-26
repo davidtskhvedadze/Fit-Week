36802c62b5c243cd4e62683e49ed2cdd4fe1a00e
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2023-09-25 21:12:42 -0400

    backend complete

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 //express app
 const app = express();
@@ -16,7 +17,13 @@ app.use((req,res,next) => {
 //routes
 app.use('/api/workouts', workoutRoutes);
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT);
-});
+//connect to DB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
