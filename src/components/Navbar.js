711dfefd3c9d45269217d7bf3459199aa20160f2
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-04-22 12:55:30 -0400

    Update user token and added signout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
- 
- return (
+  const navigate = useNavigate();
+
+  const signOut = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    navigate('/login');
+  }
+
+  return (
     <header>
         <div className="container">
           <Link to="/">
             <h1>Fit-Week</h1>
           </Link>
+          <button onClick={signOut}>Sign Out</button>
         </div>
     </header>
- )
+  )
 };
 
 export default Navbar;
