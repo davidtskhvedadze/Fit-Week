6055c09a0001218ccf645bdf8fb1349378d53fa4
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-04-22 12:49:00 -0400

    Add user schema

--- src/server/models/userModel.js
+++ src/server/models/userModel.js
@@ -3,10 +3,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username:{
-        type: String,
-        required: true
-    },
     email: {
         type: String,
         required: true
