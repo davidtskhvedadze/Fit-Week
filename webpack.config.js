bcbe612d1dbf63b0cc73ea145bc52a95b80d5958
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-04-23 12:05:17 -0400

    Add copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: ["@babel/polyfill", path.resolve(__dirname, 'src/index')],
@@ -34,7 +35,12 @@ module.exports = {
     new HTMLWebpackPlugin({
       template: path.resolve(__dirname,'src', 'index.html'),
       filename: 'index.html'
-    })
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/assets', to: 'assets' },
+      ],
+    }),
   ],
   stats:"minimal"
 };
