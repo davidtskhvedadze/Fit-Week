6905b699c2fe8765efa92697409eb41c5c7bbead
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-04-23 00:40:50 -0400

    Update Signout button style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import '../index.css';
 
 const Navbar = () => {
   const navigate = useNavigate();
 
   const signOut = () => {
     localStorage.removeItem('token');
-    localStorage.removeItem('userId');
     navigate('/login');
   }
 
   return (
     <header>
-        <div className="container">
-            <h1>Fit-Week</h1>
-          <button onClick={signOut}>Sign Out</button>
-        </div>
+      <h1>Fit-Week</h1>
+      <button onClick={signOut}>Sign Out</button>
     </header>
   )
 };
