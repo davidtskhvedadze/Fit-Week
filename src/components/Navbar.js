1db844c49b9bd254d1be61d7958a485802d78b5d
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-04-23 12:08:10 -0400

    Update navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import '../index.css';
+import '../styles/index.css';
 
 const Navbar = () => {
   const navigate = useNavigate();
